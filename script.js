6cf5e25692774574e2601a7a2cb92e6f43cfd0bb
DestinyDeCastro <destinymoon19@gmail.com>
2023-11-19 22:01:32 +0100

    all updates

--- script.js
+++ script.js
@@ -1,46 +1,69 @@
-var swiper = new Swiper ('.mySwiper-1', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+/* activacion de menu */
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+/* terminamos la activacion de menu */
+
+/* iniciamos y ocultamos el menu de buscar */
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+  document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+  document.querySelector('#search-form').classList.remove('active');
+}
+
+/*finalizacion de la iniciacion y ocultacion en el menu de buscar */
+
+
+/* iniciamos el slider */
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 135,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 45,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  loop:true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-});
-
-var swiper2 = new Swiper ('.mySwiper-2', {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        520: {
-          slidesPerView: 2,
-        },
-        950: {
-          slidesPerView: 3,
-        }
-      }
-});
-
-let tabInputs = document.querySelectorAll('.tabInput');
-
-tabInputs.forEach(function(input) {
-    input.addEventListener('change', function() {
-       let id = input.ariaValueMax;
-       let thisSwiper = document.getElementById('swiper' + id);
-       thisSwiper.swiper.update();
-    });
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
